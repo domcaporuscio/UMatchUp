3fa15c7700bcbcbec421b68c26bf1ca1151bd704
dcapor <domcaporuscio@gmail.com>
2020-11-19 20:02:41 -0500

    added VoteVC, ChallengesVC, and ProfileVC

---
+++ UMatchUp/VoteVC.swift
@@ -0,0 +1,29 @@
+//
+//  VoteVC.swift
+//  UMatchUp
+//
+//  Created by Dominic Caporuscio on 11/19/20.
+//
+
+import UIKit
+
+class VoteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Loaded VoteVC")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
